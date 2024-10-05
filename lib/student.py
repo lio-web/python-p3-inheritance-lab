c0d3ba219bd60b122b07ec0689ff057cf4a373d0
lionard <leonard.muhati@student.moringaschool.com>
2024-10-05 13:22:10 +0300

    compelete

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,12 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        # Inherit first_name and last_name from User
+        super().__init__(first_name, last_name)
+        # Initialize knowledge as an empty list
+        self.knowledge = []
+
+    def learn(self, new_knowledge):
+        # Add new knowledge to the list
+        self.knowledge.append(new_knowledge)
